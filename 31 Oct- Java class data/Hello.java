407bf9c31313d6b88a634c9a35419eb7a455788d
javabyvinti <32234213+javabyvinti@users.noreply.github.com>
2017-10-31 10:06:47 +0530

    Add files via upload

---
+++ 31 Oct- Java class data/Hello.java
@@ -0,0 +1,28 @@
+class Hello{
+
+//public --> package centric langugage
+//static --> loads first into memory, no need to create object
+//void --> return type , return nothing
+//main --> name of method
+
+	int value = 10;
+	
+	static void data(){
+
+	}
+
+	public void main(String a[])
+	{
+		System.out.println("Hello java again...");
+	}
+}
+
+
+//String, System --> pre-defined class--> where to print(console)
+//out--> object (PrintStream)
+//println("" + "\n")-->printing work
+
+
+
+
+
