4e256b86f9d5764142a40650f3c9fdc89e7b390b
javabyvinti <32234213+javabyvinti@users.noreply.github.com>
2017-12-11 10:16:31 +0530

    Add files via upload

---
+++ 11-Dec Exception Handling/java/ExceptionDemo1.java
@@ -0,0 +1,22 @@
+package com.exceptionhandling.java;
+
+public class ExceptionDemo1 {
+
+	public static void main(String[] args) {
+
+		try{
+		
+		int value = Integer.parseInt(args[0]);//"forty" -->int value =  forty
+		int result = 12/ value;
+		System.out.println(result);
+
+		}
+		catch(ArrayIndexOutOfBoundsException exception){
+			System.out.println("Please provide some value in array");
+		}
+		catch(Exception e){
+			System.out.println("Some excpetion occured");
+		}
+	}
+
+}
